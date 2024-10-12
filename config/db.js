b5ac16848961ebcabd6fd0c1d6c9ca91f21145b1
roaaayman <aymanroaa025@gmail.com>
2024-10-13 00:43:11 +0300

    No need for deprecated options

--- config/db.js
+++ config/db.js
@@ -3,10 +3,8 @@ require('dotenv').config();
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+  
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Atlas connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);

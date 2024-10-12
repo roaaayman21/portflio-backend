bd486552986b49ba311471d9f112f5882a98cda6
roaaayman <aymanroaa025@gmail.com>
2024-10-13 00:22:43 +0300

    from local to atlas

--- config/db.js
+++ config/db.js
@@ -1,7 +1,17 @@
-const mongoose=require('mongoose');
-//
-const connectDb=async ()=>{
-    await mongoose.connect('mongodb://localhost:27017/portfolio');
-    console.log('mongoDB connected');
-}
-module.exports=connectDb;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Atlas connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;

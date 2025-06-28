9d63072a54d8231b628a8d23508ae87a695cdaf3
roaaayman <aymanroaa025@gmail.com>
2025-06-28 17:00:42 +0300

    Update Project.js

--- models/Project.js
+++ models/Project.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   title: { type: String, required: true },
-  link: { type: String, required: true },
+  link: { type: String },
   photo: { type: String, required: true },
   skills: [{ type: String }],
   githubLink: { type: String }             
